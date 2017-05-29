cb589543988ea478c84d212d50d274b2fe3acbb5
Shreve-La <shrevela@gmail.com>
2017-05-29 15:46:24 -0400

    Initial Commit

---
+++ ThreeLow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreeLow
+//
+//  Created by swcl on 2017-05-29.
+//  Copyright © 2017 Shreve LaFramenta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
